c8b7034c66da9e43130267bea9698251eafb7b00
mitubakuro <nakano1113mituba@gmail.com>
2020-08-18 20:48:01 +0900

    basic認証の実装
    hogesコントローラはテスト用です

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+# Basic認証のテスト用
+resources :hoges, only:[:index]
 end
