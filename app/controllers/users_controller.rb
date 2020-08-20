9f433e85325d9a20e45fd2ff71cc93f97f201679
mitubakuro <nakano1113mituba@gmail.com>
2020-08-20 23:18:28 +0900

    pry-railsを導入

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
 
-  def sign_up
-    @user = User.new(configure_permitted_parameters)
-    @user.save
-  end
+  # def sign_up
+  #   @user = User.new(configure_permitted_parameters)
+  #   @user.save
+  # end
 
   def sign_in
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
-# Basic認証のテスト用
-resources :hoges, only:[:index]
-root to: "hoges#index"
+  # Basic認証のテスト用
+  resources :hoges, only:[:index]
+  root to: "hoges#index"
 end
