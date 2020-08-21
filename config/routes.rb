6c7d528ceac8d1633ce6ccfc1967671702b47ced
mitubakuro <nakano1113mituba@gmail.com>
2020-08-21 16:52:33 +0900

    ログイン、ログアウトを実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Basic認証のテスト用
   # resources :hoges, only:[:index]
-  # root to: "users#sign_up"
+  root to: "items#index"
 
   resources :items, only:[:index, :edit, :show, :new]
 
