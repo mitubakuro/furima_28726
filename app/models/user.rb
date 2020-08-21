c381427d190e19ce75f7f2610a05663957cb5667
mitubakuro <nakano1113mituba@gmail.com>
2020-08-21 18:29:07 +0900

    バリデーションの記載完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,14 +9,16 @@ class User < ApplicationRecord
   # has_many :orders
 
   # バリデーションの一括設定
-  # with_options presence: true do
-  #   validates :nickname
-  #   validates :birthday
-  #   validates :family_name
-  #   validates :first_name 
-  #   validates :family_kana 
-  #   validates :first_kana 
-  # end
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    validates :family_name, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :first_name , format: {with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :family_kana, format: {with: /\A[ァ-ヶー－]+\z/}
+    validates :first_kana , format: {with: /\A[ァ-ヶー－]+\z/}
+    # パスワードは半角英数字が混合
+    validates :password, format: {with:/(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]/}
+  end
 
 
 end
