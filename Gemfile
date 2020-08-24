5ddabf6595694f2f9ccd5e7940d038b59f75a3d2
mitubakuro <nakano1113mituba@gmail.com>
2020-08-24 14:51:51 +0900

    Active Storage導入とコントローラ記載、itemモデル追加

--- Gemfile
+++ Gemfile
@@ -68,3 +68,5 @@ gem "devise"
 gem "pry-rails"
 gem "gimei"
 
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,4 +10,10 @@ class ItemsController < ApplicationController
 
   def new
   end
+
+  private
+  
+  def item_params
+    params.require(:item).permit(:item_name,:item:text,:price,:deli_fee,:image).merge(user_id: current_user.id)
+  end
 end
