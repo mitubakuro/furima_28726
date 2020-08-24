ed31e6382b700d84e5f27ecbde5ea1e6cb2aa771
mitubakuro <nakano1113mituba@gmail.com>
2020-08-24 17:52:06 +0900

    バリデーション記載

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,16 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :item_name
+    validates :item_info
+    validates :item_price, format: { with:  }
+    validates :deli_fee
+    validates :condition
+    validates :category
+    validates :shipping_area
+    validates :shipping_day
+  end
 end
