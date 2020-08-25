111c60d371eadd5da9f9661dcf0ea5376c16039d
mitubakuro <nakano1113mituba@gmail.com>
2020-08-25 10:45:30 +0900

    Javascript記載の途中経過

---
+++ app/javascript/price_calc.js
@@ -0,0 +1,19 @@
+function calc(){
+  const price = document.getElementById('item-price');
+  // const profit = document.getElementById('profit');
+  price.addEventListener('input', ()=>{
+// jsだけで動かせるかも
+    // const getvalue = document.getElementById("item-price").value;
+    // const result = document.getElementById("add-tax-price");
+    // result.value = getvalue * 0.1;
+// ajax
+    // const formData = new FormData(document.getElementById("item-price"));
+    // const XHR = new XMLHttpRequest();
+    // XHR.open("POST", "/items/new", true);
+    // XHR.responseType ="json";
+    // XHR.send(formData);
+
+  });
+}
+
+window.addEventListener("load", calc);
